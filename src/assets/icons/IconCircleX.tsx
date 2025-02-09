b560f463abb67890cfbbe0c5e426ad00d09454b6
igrisdev <johanalvarezz237@gmail.com>
2025-02-09 13:27:54 -0500

    Refactor IconCircleX and IconShoppingCart components to use dynamic class names and size props for improved flexibility

--- src/assets/icons/IconCircleX.tsx
+++ src/assets/icons/IconCircleX.tsx
@@ -10,7 +10,7 @@ export const IconCircleX = ({ className }: { className?: string }) => {
       stroke-width='2'
       stroke-linecap='round'
       stroke-linejoin='round'
-      className='lucide lucide-circle-x'
+      className={className}
     >
       <circle cx='12' cy='12' r='10' />
       <path d='m15 9-6 6' />
--- src/assets/icons/IconShoppingCart.tsx
+++ src/assets/icons/IconShoppingCart.tsx
@@ -2,15 +2,14 @@ export const IconShoppingCart = ({ size = 20 }: { size?: number }) => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
-      width='24'
-      height='24'
+      width={size}
+      height={size}
       viewBox='0 0 24 24'
       fill='none'
       stroke='currentColor'
       stroke-width='2'
       stroke-linecap='round'
       stroke-linejoin='round'
-      className='lucide lucide-shopping-cart'
     >
       <circle cx='8' cy='21' r='1' />
       <circle cx='19' cy='21' r='1' />
