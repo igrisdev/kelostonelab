fa867b5e22d30aa1ff7509ed9cb760ab7b5e2294
igrisdev <johanalvarezz237@gmail.com>
2025-02-09 12:55:56 -0500

    Refactor ButtonAddToCart component to support small size variant and integrate it into DeferFeaturedProducts for improved cart functionality

--- src/components_functional/ButtonAddToCart.tsx
+++ src/components_functional/ButtonAddToCart.tsx
@@ -1,10 +1,22 @@
+import { IconShoppingCart } from '@assets/icons/IconShoppingCart'
 import { useStoreCart } from '@stores/cartStore'
 
-export const ButtonAddToCart = () => {
+export const ButtonAddToCart = ({ small = false }) => {
   const { setCart } = useStoreCart()
 
+  const handleClick = () => {
+    setCart()
+  }
+
+  if (small)
+    return (
+      <button className='btn btn-ghost btn-square btn-sm' onClick={handleClick}>
+        <IconShoppingCart size={17} />
+      </button>
+    )
+
   return (
-    <button className='btn btn-accent rounded-none' onClick={() => setCart()}>
+    <button className='btn btn-accent rounded-none' onClick={handleClick}>
       Add to cart
     </button>
   )
